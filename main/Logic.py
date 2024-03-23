70d787778d0e99f91435b883db44e3f39384ef2c
Andrew James Sass <ajsass@email.wm.edu>
2024-03-23 17:38:27 -0400

    logic in gui

--- main/Logic.py
+++ main/Logic.py
@@ -11,6 +11,8 @@ class Logic:
         self.deck.readIn(link)
     def addCard (self, term, definition):
         self.deck.AddCard(term,definition)
+    def DeckSize(self):
+        return len(self.deck.getDeck())
     
     
 
