b94a0b9d263f0a666cd4c29fa8bafb7d45f44bde
Andrew James Sass <ajsass@email.wm.edu>
2024-03-24 00:05:44 -0400

    gfx 90% done

--- main/imageGet.py
+++ main/imageGet.py
@@ -23,5 +23,32 @@ def getloadanstufFlash():
     image2 = PhotoImage(file=img2)
     return image,image2
 
+def getGeneric(text):
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    
+    img = parent_directory+"\\resources\\"+text+".png"
+    image = PhotoImage(file=img)
+    
+    return image
 
+def getback():
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    
+    img = parent_directory+"\\resources\\back.png"
 
+    image = PhotoImage(file=img)
+    
+    return image
+
+def getselect():
+    current_directory = os.getcwd()
+    parent_directory = os.path.dirname(current_directory)
+    
+    img = parent_directory+"\\resources\\learn.png"
+    img2 = parent_directory+"\\resources\\study.png"
+
+    image = PhotoImage(file=img)
+    image2 = PhotoImage(file=img2)
+    return image,image2
